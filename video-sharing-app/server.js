2e5c27794f87542af55b655d336ba7932e036f6c
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2024-01-11 10:31:21 +0100

    new updates

--- video-sharing-app/server.js
+++ video-sharing-app/server.js
@@ -2,15 +2,19 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
 
 require('dotenv').config({path: './config/.env'})
 
 connectDB()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 
-
+app.use('/', mainRoutes)
 
 
 
